662a02fe7bc82576c293221f74ab692abff8d5be
Utsav Ghimire <ghimireutsav11@gmail.com>
2022-08-21 01:19:39 +0545

    no words for main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_project/screens/create.dart';
+import 'package:flutter_project/screens/homepage.dart';
+import 'package:flutter_project/screens/option_screen.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'screens/waiting_lobby.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,7 +21,28 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const CreateRoom(),
+      home: const OptionScreen(),
+      onGenerateRoute: (settings) {
+        switch (settings.name) {
+          case OptionScreen.routeName:
+            return MaterialPageRoute(
+              builder: (_) => const OptionScreen(),
+            );
+          case CreateRoom.routeName:
+            return MaterialPageRoute(
+              builder: (_) => CreateRoom(
+                isJoin: settings.arguments as bool,
+              ),
+            );
+          case WaitingLobby.routeName:
+            return MaterialPageRoute(builder: (_) => const WaitingLobby());
+          case Homepage.routeName:
+            return MaterialPageRoute(builder: (_) => const Homepage());
+
+          default:
+        }
+        return null;
+      },
     );
   }
 }
