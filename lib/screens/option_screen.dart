a68d3802781dbb7f9dea8f43ab440d79b26c65c0
Utsav Ghimire <ghimireutsav11@gmail.com>
2022-08-21 01:14:50 +0545

    final commit for option selection screen

---
+++ lib/screens/option_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_project/screens/create.dart';
+
+import '../widgets/button.dart';
+
+class OptionScreen extends StatelessWidget {
+  static const String routeName = '/optionScreen';
+  const OptionScreen({Key? key}) : super(key: key);
+  void createRoom(BuildContext context, {bool isJoin = false}) {
+    Navigator.pushNamed(
+      context,
+      CreateRoom.routeName,
+      arguments: isJoin,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CustomButton(
+              onTap: () => createRoom(context),
+              text: 'Create Room',
+            ),
+            const SizedBox(height: 20),
+            CustomButton(
+              onTap: () => createRoom(context, isJoin: true),
+              text: 'Join Room',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
