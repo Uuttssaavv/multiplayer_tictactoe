8b0b868349190b3fe19893db244c17046e819ce2
Utsav Ghimire <ghimireutsav11@gmail.com>
2022-08-22 14:36:17 +0545

    created game data classes

---
+++ lib/models/game_data.dart
@@ -0,0 +1,64 @@
+import 'package:flutter_project/models/player_model.dart';
+
+abstract class GameData {
+  Map<String, dynamic>? roomData;
+  List<String> displayElements;
+  int filledBoxes;
+  Player? player1;
+  Player? player2;
+  String errorMessage;
+  bool isCreator;
+  GameData({
+    this.roomData,
+    this.displayElements = const [],
+    this.filledBoxes = 0,
+    this.errorMessage = '',
+    this.isCreator = false,
+    this.player1,
+    this.player2,
+  });
+}
+
+class CreateRoomData extends GameData {
+  CreateRoomData({super.roomData});
+}
+
+class JoinRoomData extends GameData {
+  JoinRoomData({
+    super.roomData,
+    super.isCreator,
+  });
+}
+
+class JoinRoomErrorData extends GameData {
+  JoinRoomErrorData({
+    super.errorMessage,
+  });
+}
+
+class UpdatePlayerData extends GameData {
+  UpdatePlayerData({
+    super.player1,
+    super.player2,
+  });
+}
+
+class UpdateRoomData extends GameData {
+  UpdateRoomData({
+    super.roomData,
+  });
+}
+
+class UpdateGameData extends GameData {
+  UpdateGameData({
+    super.roomData,
+    super.displayElements,
+  });
+}
+
+class EndGameData extends GameData {
+  final String winner;
+  final String ponts;
+
+  EndGameData({required this.winner, required this.ponts});
+}
